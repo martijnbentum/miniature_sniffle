6884ef1febf0ea5e0af13cd9ca4383e7bbc65e60
martijnbentum <bentummartijn@gmail.com>
2022-06-16 15:37:33 +0200

    bash script to log gpu information for gpus on all gpu ponies

---
+++ ssh_gpu_logger.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ssh mbentum@mistmane /vol/tensusers/mbentum/AUDIOSERVER/repo/gpu_logger.sh
+ssh mbentum@rarity /vol/tensusers/mbentum/AUDIOSERVER/repo/gpu_logger.sh
+ssh mbentum@snips /vol/tensusers/mbentum/AUDIOSERVER/repo/gpu_logger.sh
+ssh mbentum@thunderlane /vol/tensusers/mbentum/AUDIOSERVER/repo/gpu_logger.sh
