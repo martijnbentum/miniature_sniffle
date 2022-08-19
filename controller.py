bf7896d2720700284a6bb7a8f73b24d029ee1a00
martijnbentum <bentummartijn@gmail.com>
2022-08-19 15:00:51 +0200

    moved show log to logger module; added get_gpu_selector to search for available gpu until available

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 import argument_handler 
 import gpu_selector as gpus
+import logger
 import os
 import ponies
 import sys
@@ -24,52 +25,40 @@ def make_ssh_command(gpu,args):
     m += 'python /vol/tensusers/mbentum/AUDIOSERVER/repo/wav2vec2.py '
     m += '-device ' + str(device) + argument_command + '"'
     return m
-    
 
-def transcribe(args = None, verbose = False):
+def show_log_command(gpu):
+    m = 'python /vol/tensusers/mbentum/AUDIOSERVER/repo/logger.py '
+    m += '-server_name ' + gpu.name + ' -device ' + str(gpu.device)
+    return m
+    
+    
+def get_gpus_selector():
+    '''create gpu selector object, return if there are available gpus,
+    otherwise wait and try again.
+    '''
     s = gpus.Selector()
+    if not s.selected_gpu:
+        print('could not find an available gpu')
+        print('\n'.join(map(str,s.rejected)))
+        print('waiting a minute to try again')
+        time.sleep(60)
+        return get_gpus_selector()
+    return s
+
+def transcribe(args = None, verbose = True):
+    s = get_gpus_selector()
     device = s.selected_gpu.device
     server_name = s.selected_gpu.name
     print('selected gpu: ',server_name,pony_dict[server_name],
         ', device:',device)
     ssh_command = make_ssh_command(s.selected_gpu,args)
-    if verbose: print(ssh_command)
+    if verbose: print('ssh command:',ssh_command)
     start = time.time()
-    x = threading.Thread(target = show_log, args = (server_name,device,start))
+    x = threading.Thread(target = logger.show_log, 
+        args = (server_name,device,start))
     x.start()
     os.system(ssh_command)
-    show_log(server_name,device,start)
 
-def show_log(server_name,device, start = None):
-    if not start: start = time.time()
-    filename = log_dir + server_name + '_' + str(device)
-    print('showing log',filename)
-    if not os.path.isfile(filename):
-        m ='cannot show log file, does not exist'
-        raise FileNotFoundError(filename,m)
-    lines = []
-    while True:
-        new = _check_log(filename,start,lines)
-        done = False
-        for line in new:
-            print(line)
-            lines.append(line)
-            if 'setting status: closed' in line: done = True
-        if done: 
-            print('transcription is done, stop showing log file')
-            break
-        time.sleep(1)
-
-def _check_log(filename, start_time,read_lines):
-    with open(filename) as fin:
-        lines = fin.read().split('\n')
-    selection = []
-    for line in lines:
-        if not line:continue
-        if line in read_lines: continue
-        line_time =  int(line.split('\t')[1]) 
-        if line_time > start_time: selection.append(line)
-    return selection
 
 
 
